675de51e6f741dfa5711f778657f7db33a70f57e
Reut Vladislav <reut-2011@mail.ru>
2024-11-15 11:31:42 +0300

    feat: add dark mode to components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import MainSection from "./components/MainSection/MainSection";
+import { useState } from "react";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  function toggleDarkMode() {
+    setDarkMode((prevMode) => !prevMode);
+  }
+
   return (
     <div className="dark-mode">
-      <Navbar />
-      <MainSection />
+      <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      <MainSection darkMode={darkMode} />
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,6 +6,14 @@ export default function Navbar(props) {
     <nav className={props.darkMode ? "dark" : ""}>
       <img className="nav__logo_icon" src={NavbarLogo} />
       <h3 className="nav__logo_text">ReactFacts</h3>
+
+      <div className="toggler">
+        <p className="toggler__light">Light</p>
+        <div className="toggler__slider" onClick={props.toggleDarkMode}>
+          <div className="toggler__slider_circle"></div>
+        </div>
+        <p className="toggler__dark">Dark</p>
+      </div>
     </nav>
   );
 }
